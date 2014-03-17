e908dc94160e97f60a7a412cbe9d5fea831a7e47
Pedro Noel <pedro@whistle.is>
2014-03-17 11:31:17 +0100

    Create torcheck.php

---
+++ torcheck.php
@@ -0,0 +1,18 @@
+<?php
+
+function IsTorExitPoint(){
+if (gethostbyname(ReverseIPOctets($_SERVER['REMOTE_ADDR']).".".$_SERVER['SERVER_PORT'].".".ReverseIPOctets($_SERVER['SERVER_ADDR']).".ip-port.exitlist.torproject.org")=="127.0.0.2") {
+return true;
+} else {
+return false;
+} 
+}
+function ReverseIPOctets($inputip){
+$ipoc = explode(".",$inputip);
+return $ipoc[3].".".$ipoc[2].".".$ipoc[1].".".$ipoc[0];
+}
+
+if ( IsTorExitPoint() == true ) {
+include 'tor.html';
+} else { include 'notor.html' ; }
+?>
